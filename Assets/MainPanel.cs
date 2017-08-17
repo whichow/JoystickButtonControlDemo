0cce117456ac104318963723724b495fd833087a
liuyuchun@orbbec.com <liuyuchun@orbbec.com>
2017-08-17 10:52:13 +0800

    焦点控制demo

---
+++ Assets/MainPanel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour {
+
+	public Button button_U_L;
+	public Button button_U_R;
+	public Button button_D_L;
+	public Button button_D_R;
+
+	public PopupPanel popupPanel;
+
+	// Use this for initialization
+	void Start () {
+		button_U_L.Select();
+		Button[] buttons = new [] { button_U_L, button_U_R, button_D_L, button_D_R };
+		foreach(var button in buttons)
+		{
+			button.onClick.AddListener (()=>{
+				popupPanel.gameObject.SetActive(true);
+				popupPanel.centerButton.Select();
+				popupPanel.onDisable.AddListener(()=>{
+					button_U_L.Select();
+				});
+			});
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PopupPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class PopupPanel : MonoBehaviour {
+
+	public Button centerButton;
+	public UnityEvent onDisable;
+
+//	void OnEnable() {
+//		centerButton.Select ();
+//	}
+
+	void OnDisable() {
+		onDisable.Invoke ();
+	}
+	// Use this for initialization
+	void Start () {
+		centerButton.onClick.AddListener (() => {
+			gameObject.SetActive (false);
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
